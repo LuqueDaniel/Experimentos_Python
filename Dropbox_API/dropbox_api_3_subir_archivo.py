7341d40b03d24a5b7bcba21b5536abd2337ae7fd
Daniel Luque <danielluque14@gmail.com>
2012-06-13 14:22:35 +0200

    Añadido el ejemplo 3 d ela API de Dropbox

---
+++ Dropbox_API/dropbox_api_3_subir_archivo.py
@@ -0,0 +1,23 @@
+#ecoding: utf-8
+
+from dropbox import client, session, rest
+
+app_key = '' #APP KEY
+app_secret = '' #APP_SECRET
+app_type = 'app_folder'
+
+sesion = session.DropboxSession(app_key, app_secret, app_type)
+request_token = sesion.obtain_request_token()
+
+url = sesion.build_authorize_url(request_token)
+print "url:", url
+print "Visite la URL de arriba y permita la aplicación, a continuación presione intro"
+raw_input()
+
+access_token = sesion.obtain_access_token(request_token)
+
+cliente = client.DropboxClient(sesion)
+
+archivo = 'muestra.txt'
+subiendo = cliente.put_file('/muestra1.txt', archivo)
+print "Subiendo: %s" % (subiendo)
